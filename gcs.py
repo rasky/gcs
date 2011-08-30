6a77c03c4a7bb53c78707941e1daf3dbe774ca5c
Giovanni Bajo <rasky@develer.com>
2011-08-30 12:44:49 +0200

    Cosmetics and comments

--- gcs.py
+++ gcs.py
@@ -6,13 +6,20 @@ import struct
 from array import array
 
 def bitwriter(f):
+    """
+    Coroutine to write bits into a file-like.
+    Use .send((N, V)) to write the N least-significant
+    bits of V into the file.
+    Remember to call .close() to flush the internal
+    state when done.
+    """
     v = 0
     n = 0
     try:
         while 1:
             (n2,v2) = yield None
             v <<= n2
-            v |= v2
+            v |= v2 & ((1<<n2)-1)
             n += n2
             while n >= 8:
                 b = (v >> (n-8)) & 255
@@ -20,13 +27,16 @@ def bitwriter(f):
                 n -= 8
             v &= 255
     except GeneratorExit:
-        print "flushing"
         if n != 0:
             v &= (1 << n) - 1
             f.write(chr(v))
         raise
 
 def bitreader(f):
+    """
+    Coroutine to read bits from a file-like.
+    Use .send(N) to read N bits from the file.
+    """
     accum = 0
     n = 0
     v = None
@@ -37,6 +47,7 @@ def bitreader(f):
             try:
                 accum |= ord(f.read(1))
             except TypeError:
+                # ord(None) => eof
                 return
             n += 8
         v = (accum >> (n-n2)) & ((1<<n2)-1)
@@ -45,6 +56,13 @@ def bitreader(f):
 
 
 def golomb_enc(f, P):
+    """
+    Coroutine to encode Golomb/Rice-encoded values into
+    a file-like f, with parameter P.
+    Use .send(V) to encode the value V into the file.
+    This is an optimal encoding if values are geometrically
+    distributed with P.
+    """
     logp = int(math.log(P,2))
     f = bitwriter(f); f.next();
     while 1:
@@ -54,6 +72,11 @@ def golomb_enc(f, P):
         f.send((logp, r))   
 
 def golomb_dec(f, P):
+    """
+    Coroutine to decode Golomb/Rice-encoded values
+    from a file-like f, with parameter P.
+    Use .next() (or iterate over it) to read the values.
+    """
     logp = int(math.log(P,2))
     f = bitreader(f); f.next()
     while 1:
@@ -66,8 +89,14 @@ def golomb_dec(f, P):
             return
         yield v
 
-
 def gcs_hash(w, (N,P)):
+    """
+    Hash value for a GCS with N elements and 1/P probability
+    of false positives.
+    We just need a hash that generates uniformally-distributed
+    values for best results, so any crypto hash is fine. We
+    default to MD5.
+    """
     h = md5(w).hexdigest()
     h = long(h,16)
     return h % (N*P)
@@ -107,11 +136,11 @@ class GCSQuery:
         N = 0
         self._rewind()
         for d in golomb_dec(self.f, self.P):
-            #print h, h2
             N += d
             if h == N:
                 return True
             if h < N:
+                # early-exit since values are sorted
                 return False
         return False
             
@@ -125,15 +154,13 @@ if __name__ == "__main__":
             gcs.add(w.strip())
         with open("table.gcs", "wb") as f:
             gcs.finalize(f)
-            print "Number of words: %d" % len(words)
-            print "False positives: %f" % (1/prob)
-            print "Size: %d" % (f.tell())
-            print "Bits per word: %f" % (f.tell()*8 / len(words))
+            fsize = f.tell()
+        print "Number of words: %d" % len(words)
+        print "False positives: %f" % (1/prob)
+        print "Size: %d" % (fsize)
+        print "Bits per word: %f" % (fsize*8 / len(words))
 
     elif sys.argv[1] == "query":
-        import time
-        a = time.time()
         gcs = GCSQuery(open("table.gcs","rb"))
         for w in sys.argv[2:]:
             print gcs.query(w)
-        print "Time:", time.time() - a
