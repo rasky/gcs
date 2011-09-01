94f7f1b0494c3a9b79decc9859dc6fce9f839f21
Giovanni Bajo <rasky@develer.com>
2011-09-01 20:36:03 +0200

    Implement query support in C++.

--- cpp/gcs.cpp
+++ cpp/gcs.cpp
@@ -4,10 +4,12 @@
 #include <stdint.h>
 #include <string.h>
 #include <algorithm>
+#include <assert.h>
 
 #define BITMASK(n)    ((1 << (n)) - 1)
 
-static unsigned int gcs_hash(const void *data, int size, int N, int P)
+
+static uint32_t gcs_hash(const void *data, int size, int N, int P)
 {
 	unsigned char digest[16];
 	MD5Context ctx;
@@ -16,35 +18,38 @@ static unsigned int gcs_hash(const void *data, int size, int N, int P)
 	MD5Update(&ctx, (unsigned char*)data, size);
 	MD5Final(digest, &ctx);
 
-	uint32_t dignum = 
-		((uint32_t)digest[12]<<24) |
-		((uint32_t)digest[13]<<16) |
-		((uint32_t)digest[14]<<8) |
-		((uint32_t)digest[15]<<0);
+	hash_t dignum = 0;
+	for (int i = 16-sizeof(hash_t); i < 16; ++i)
+	{
+		dignum <<= 8;
+		dignum += digest[i];
+	}
+
 	return dignum % (N*P);
 }
 
 static int floor_log2(int v)
 {
-	return 32 - __builtin_clz(v-1);
+	return sizeof(int)*8 - __builtin_clz(v-1);
 }
 
 /**************************************************************/
 
-class BitEncoder
+class BitWriter
 {
 private:
 	std::ostream &f;
-	long accum;
+	unsigned long accum;
 	int n;
-	enum { ACCUM_BITS = sizeof(long)*8 };
+	enum { ACCUM_BITS = sizeof(unsigned long)*8 };
 
 public:
-	BitEncoder(std::ostream &_f)
+	BitWriter(std::ostream &_f)
 		: f(_f), accum(0), n(0) {}
 
-	void encode(int nbits, int value)
+	void write(int nbits, unsigned value)
 	{
+		assert(nbits >= 0);
 		while (nbits)
 		{
 			int nb = std::min(ACCUM_BITS-n, nbits);
@@ -56,8 +61,7 @@ public:
 
 			while (n >= 8)
 			{
-				unsigned char ch = (accum >> (n-8)) & BITMASK(8);
-				f.write((char*)&ch, 1);
+				f.put((accum >> (n-8)) & BITMASK(8));
 				n -= 8;
 				accum &= BITMASK(n);
 			}
@@ -68,8 +72,8 @@ public:
 	{
 		if (n > 0)
 		{
-			unsigned char ch = accum;
-			f.write((char*)&ch, 1);
+			assert(n < 8);
+			f.put(accum & BITMASK(8));
 			n = 0;
 			accum = 0;
 		}
@@ -80,7 +84,7 @@ public:
 class GolombEncoder
 {
 private:
-	BitEncoder f;
+	BitWriter f;
 	int P, log2P;
 
 public:
@@ -88,15 +92,16 @@ public:
 		: f(_f), P(_P)
 	{
 		log2P = floor_log2(P);
+		assert(log2P > 0);
 	}
 
-	void encode(unsigned int value)
+	void encode(hash_t value)
 	{
-		int q = value / P;
-		int r = value - q*P;
+		hash_t q = value / P;
+		hash_t r = value - q*P;
 
-		f.encode(q+1, BITMASK(q)<<1);
-		f.encode(log2P, r);
+		f.write(q+1, BITMASK(q)<<1);
+		f.write(log2P, r);
 	}
 
 	void flush(void)
@@ -109,13 +114,14 @@ public:
 GCSBuilder::GCSBuilder(int _n, int _p)
 	: N(_n), P(_p)
 {
+	assert(N*P <= ~(hash_t)0);
 	values.reserve(N*P);
 	values.push_back(0);
 }
 
 void GCSBuilder::add(const void *data, int size)
 {
-	unsigned h = gcs_hash(data, size, N, P);
+	hash_t h = gcs_hash(data, size, N, P);
 	values.push_back(h);
 }
 
@@ -123,7 +129,7 @@ void GCSBuilder::finalize(std::ostream& f)
 {
 	std::sort(values.begin(), values.end());
 
-	int v = O32_HOST_TO_BE(N);
+	int32_t v = O32_HOST_TO_BE(N);
 	f.write((char*)&v, 4);
 	v = O32_HOST_TO_BE(P);
 	f.write((char*)&v, 4);
@@ -131,7 +137,7 @@ void GCSBuilder::finalize(std::ostream& f)
 	GolombEncoder ge(f, P);
 	for (int i=0; i<(int)values.size()-1; ++i)
 	{
-		int diff = values[i+1] - values[i];
+		hash_t diff = values[i+1] - values[i];
 		if (diff != 0)
 			ge.encode(diff);
 	}
@@ -141,6 +147,103 @@ void GCSBuilder::finalize(std::ostream& f)
 
 /**************************************************************/
 
+class BitReader
+{
+private:
+	std::istream &f;
+	unsigned long accum;
+	int n;
+
+public:
+	BitReader(std::istream &f_)
+		: f(f_), accum(0), n(0)
+	{}
+
+	bool eof(void)
+	{
+		return f.eof();
+	}
+
+	unsigned int read(int nbits)
+	{
+		while (n < nbits)
+		{
+			accum <<= 8;
+			accum |= f.get();
+			n += 8;
+		}
+
+		unsigned int ret = (accum >> (n-nbits));
+		n -= nbits;
+		accum &= BITMASK(n);
+		return ret;
+	}
+};
 
 
+class GolombDecoder
+{
+	BitReader f;
+	int P, log2P;
+
+public:
+	GolombDecoder(std::istream &f_, int P_)
+		: f(f_), P(P_)
+	{
+		log2P = floor_log2(P);
+	}
+
+	bool eof(void)
+	{
+		return f.eof();
+	}
+
+	hash_t next(void)
+	{
+		hash_t v = 0;
+		while (f.read(1))
+		{
+			v += P;
+			if (f.eof())
+				return 0;
+		}
+		v += f.read(log2P);
+		return v;
+	}
+};
+
+GCSQuery::GCSQuery(std::istream &f_)
+	: f(f_)
+{
+	int32_t v;
+
+	f.read((char*)&v, 4);
+	N = O32_BE_TO_HOST(v);
+
+	f.read((char*)&v, 4);
+	P = O32_BE_TO_HOST(v);
+}
+
+bool GCSQuery::query(const void *data, int size)
+{
+	unsigned h = gcs_hash(data, size, N, P);
+	unsigned int value = 0;
+	
+	f.seekg(8);
+	f.clear();
+	
+	GolombDecoder gd(f, P);
+	while (!gd.eof())
+	{
+		unsigned int diff = gd.next();
+		value += diff;
+
+		if (value == h)
+			return true;
+		else if (value > h)
+			return false;
+	}
+
+	return false;
+}
 
--- cpp/gcs.h
+++ cpp/gcs.h
@@ -4,16 +4,28 @@
 #include <vector>
 #include <iostream>
 
+typedef uint32_t hash_t;
+
 class GCSBuilder
 {
 	int N, P;
-	std::vector<int> values;
+	std::vector<hash_t> values;
 
 public:
 	GCSBuilder(int N, int P);
 	void add(const void *data, int size);
 	void finalize(std::ostream &f);
 };	
+
+class GCSQuery
+{
+	int N, P;
+	std::istream &f;
+
+public:
+	GCSQuery(std::istream &f);
+	bool query(const void *data, int size);
+};
 
 
 #endif /* GCS_H */
